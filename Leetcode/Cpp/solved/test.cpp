ea919a8998c3a56748290960ddfca0ddec2e8e45
Luis Ernesto <luisernestoserrras@gmail.com>
2024-09-22 03:17:26 -0400

    cpp start

---
+++ Leetcode/Cpp/solved/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1, n2;
+    cout<<"Digite un numero: "; cin>>n1;
+    cout<<"Digite otro numero: "; cin>>n2;
+    cout<<"La suma es: "<<n1+n2<<endl<<"La resta es: "<<n1-n2<<endl<<"La multiplicacion es: "<<n1*n2<<endl<<"La division es: "<<n1/n2<<endl;
+    return 0;
+}
