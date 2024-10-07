c903c105b770f89042730df1f66d80ea74c73ad6
Luis Ernesto <luisernestoserras@gmail.com>
2024-10-07 02:48:32 -0400

    54, 55 & 56

---
+++ Leetcode/Python/solved/2696-Minimum String Lenght After Removing Substrings.py
@@ -0,0 +1,41 @@
+#2696 Minimum String Lenght After Removing Substrings
+'''
+You are given a string s consisting only of uppercase English letters.
+
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+Return the minimum possible length of the resulting string that you can obtain.
+
+Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+ 
+
+Example 1:
+
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+Example 2:
+
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains the same.
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists only of uppercase English letters.
+'''
+class Solution:
+    def minLength(self, s: str) -> int:
+        while "AB" in s or "CD" in s: s = str(str(s.split("AB")).replace("['", "").replace("', '", "").replace("''", "").replace("']","").split("CD")).replace("['", "").replace("', '", "").replace("''", "").replace("']","")
+        return len(s)
+s = "ACBBDACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDBACDB"
+x = Solution()
+x.minLength(s)
