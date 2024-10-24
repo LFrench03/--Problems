77fa06aa57a6a7829a856b008e02f05ccf8bf0cb
Luis Ernesto <luisernestoserras@gmail.com>
2024-10-24 02:46:59 -0400

    plus 4

---
+++ Leetcode/Python/solved/300-Longest Increasing Subsequence.py
@@ -0,0 +1,38 @@
+#300 Longest Increasing Subsequence
+'''
+Given an integer array nums, return the length of the longest strictly increasing 
+subsequence.
+
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+'''
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        n = len(nums)
+        dp = [1] * n 
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1: 
+                    dp[i] = dp[j] + 1
+        return max(dp) 
+
+
