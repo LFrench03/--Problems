5cbdc0f018fd86419651245cc35805baf67ba679
Luis Ernesto <luisernestoserras@gmail.com>
2024-11-02 06:30:31 -0400

    ???-165

---
+++ Leetcode/Python/solved/148-Sort List.py
@@ -0,0 +1,49 @@
+#148 Sort List
+'''
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head: return
+        arr, temp = [], head
+        while temp:
+            arr.append(temp.val)
+            temp = temp.next
+        i, temp = 0, head
+        arr.sort()
+        while temp:
+            temp.val = arr[i]
+            i+=1
+            temp = temp.next
+        return head
